82fd0e561c335380d3e6f93fd4e5095f816770e4
Matthew Perry <perrygeo@users.noreply.github.com>
2025-06-29 18:10:23 -0600

    Misc updates (#21)
    * docker volume
    
    * modify example
    
    * bb
    
    * clean
    
    * update versions

--- examples/pgbench_tutorial.clj
+++ examples/pgbench_tutorial.clj
@@ -47,25 +47,21 @@
   (show
    (filter #(not (:system_object %)) f)))
 
-^:kindly/hide-code
 (defn meta-as-header [x]
-  (kind/md (str "### " (:doc (meta x)))))
+  (kind/md (str "### " x "\n" (:doc (meta x)))))
 
-;;
 ;; ## Setup
-;;
 
 ;; ### Dependencies
 
-^:kindly/hide-code
-(kind/md
- "```clojure
- {:deps {com.layerware/hugsql {:mvn/version \"0.5.3\"}
-         com.layerware/hugsql-adapter-next-jdbc {:mvn/version \"0.5.3\"}
-         org.scicloj/clay {:mvn/version \"2-beta11\"}
-         org.postgresql/postgresql {:mvn/version \"42.7.3\"}
-         seancorfield/next.jdbc {:mvn/version \"1.2.659\"}}}
-  ```")
+; ```clojure
+;  {:deps 
+;  {com.layerware/hugsql {:mvn/version "0.5.3"}
+;          com.layerware/hugsql-adapter-next-jdbc {:mvn/version "0.5.3"}
+;          org.scicloj/clay {:mvn/version "2-beta11"}
+;          org.postgresql/postgresql {:mvn/version "42.7.3"}
+;          seancorfield/next.jdbc {:mvn/version "1.2.659"}}}
+; ```
 
 ; ### Database benchmark
 
@@ -76,19 +72,15 @@
 
 ; To initialize
 
-^:kindly/hide-code
-(kind/md
- "```bash
- PGPASSWORD=password pgbench --host localhost --port 5432 --username postgres -s 10 -F 100 -i -I dtgvpf main
- ```")
+; ```bash
+; PGPASSWORD=password pgbench --host localhost --port 5432 --username postgres -s 10 -F 100 -i -I dtgvpf main
+; ```
 
 ; Then run the benchmarks
 
-^:kindly/hide-code
-(kind/md
- "```bash
- PGPASSWORD=password pgbench --host localhost --port 5432 --username postgres --client 32 --transactions 1000 --jobs 8 main
- ```")
+; ```bash
+; PGPASSWORD=password pgbench --host localhost --port 5432 --username postgres --client 32 --transactions 1000 --jobs 8 main
+; ```
 
 ;; ### Driver Setup 
 
@@ -100,22 +92,18 @@
 
 ;; or from a JDBC URI.
 
-(def db2
+(def db-from-uri
   (jdbc/get-datasource
    "jdbc:postgresql://localhost:5432/main?user=postgres&password=password"))
 
 ;; Independently, we need to tell hugsql to expect next-jdbc.
+
 (hugsql/set-adapter! (next-adapter/hugsql-adapter-next-jdbc))
 
 ;; Do a health check to ensure connectivity
 
-
-^:kindly/hide-code
-(comment
-  (pgex/health-check db2))
-
 (pgex/health-check db)
-
+(pgex/health-check db-from-uri)
 
 ; ## Settings at a glance
 
@@ -134,6 +122,7 @@
 ; Here's my configuration tuned for test/dev on my laptop.
 
 (show (pgex/db-settings db))
+
 ; example row
 ^:kindly/hide-code (first (pgex/db-settings db))
 
@@ -149,37 +138,37 @@
 ^:kindly/hide-code (meta-as-header #'pgex/databases)
 (show (pgex/databases db))
 ; example row
-^:kindly/hide-code (first (:databases dd))
+(first (:databases dd))
 
 ^:kindly/hide-code (meta-as-header #'pgex/schemas)
 (show-public (pgex/schemas db))
 ; example row
-^:kindly/hide-code (first (:schemas dd))
+(first (:schemas dd))
 
 ^:kindly/hide-code (meta-as-header #'pgex/views)
 (show-public (pgex/views db))
 ; example row
-^:kindly/hide-code (first (:views dd))
+(first (:views dd))
 
 ^:kindly/hide-code (meta-as-header #'pgex/indexes)
 (show-public (pgex/indexes db))
 ; example row
-^:kindly/hide-code (first (:indexes dd))
+(first (:indexes dd))
 
 ^:kindly/hide-code (meta-as-header #'pgex/columns)
 (show-public (pgex/columns db))
 ; example row
-^:kindly/hide-code (first (:columns dd))
+(first (:columns dd))
 
 ^:kindly/hide-code (meta-as-header #'pgex/tables)
 (show-public (pgex/tables db))
 ; example row
-^:kindly/hide-code (first (:tables dd))
+(first (:tables dd))
 
 ^:kindly/hide-code (meta-as-header #'pgex/functions)
 (show-public (pgex/functions db))
 ; example row
-^:kindly/hide-code (first (:functions dd))
+(first (:functions dd))
 
 #_(filter #(= (:t_name %) "pgbench_accounts") (pgex/tables db))
 
@@ -190,127 +179,120 @@
 (def stats (pgex/read-stats db {:limit 100}))
 (keys stats)
 
-
 ^:kindly/hide-code (meta-as-header #'pgex/vacuum-stats)
 (show (pgex/vacuum-stats db))
 ; example row
-^:kindly/hide-code (first (:vacuum-stats stats))
+(first (:vacuum-stats stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/index-usage)
 (show (pgex/index-usage db))
 ; example row
-^:kindly/hide-code (first (:index-usage stats))
+(first (:index-usage stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/total-index-size)
 (pgex/total-index-size db)
 ; example row
-^:kindly/hide-code (first (:total-index-size stats))
+(first (:total-index-size stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/cache-hit)
 (show (pgex/cache-hit db))
 ; example row
-^:kindly/hide-code (first (:cache-hit stats))
+(first (:cache-hit stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/records-rank)
 (show (pgex/records-rank db))
 ; example row
-^:kindly/hide-code (first (:records-rank stats))
+(first (:records-rank stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/index-cache-hit)
 (show (pgex/index-cache-hit db))
 ; example row
-^:kindly/hide-code (first (:index-cache-hit stats))
+(first (:index-cache-hit stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/outliers)
 (show (pgex/outliers db {:limit 10}))
 ; example row
-^:kindly/hide-code (first (:outliers stats))
-
+(first (:outliers stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/extensions)
 (show (pgex/extensions db))
 ; example row
-^:kindly/hide-code (first (:extensions stats))
+(first (:extensions stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/total-table-size)
 (show (pgex/total-table-size db))
 ; example row
-^:kindly/hide-code (first (:total-table-size stats))
+(first (:total-table-size stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/bloat)
 (show (pgex/bloat db))
 ; example row
-^:kindly/hide-code (first (:bloat stats))
+(first (:bloat stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/calls)
 (show (pgex/calls db {:limit 10}))
 ; example row
-^:kindly/hide-code (first (:calls stats))
+(first (:calls stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/table-size)
 (show (pgex/table-size db))
 ; example row
-^:kindly/hide-code (first (:table-size stats))
+(first (:table-size stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/connections)
 (show (pgex/connections db))
 ; example row
-^:kindly/hide-code (first (:connections stats))
+(first (:connections stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/table-cache-hit)
 (show (pgex/table-cache-hit db))
 ; example row
-^:kindly/hide-code (first (:table-cache-hit stats))
+(first (:table-cache-hit stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/table-indexes-size)
 (show (pgex/table-indexes-size db))
 ; example row
-^:kindly/hide-code (first (:table-indexes-size stats))
-
+(first (:table-indexes-size stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/seq-scans)
 (show (pgex/seq-scans db))
 ; example row
-^:kindly/hide-code (first (:seq-scans stats))
+(first (:seq-scans stats))
 
 ^:kindly/hide-code (meta-as-header #'pgex/index-size)
 (show (pgex/index-size db))
 ; example row
-^:kindly/hide-code (first (:index-size stats))
+(first (:index-size stats))
 
-^:kindly/hide-code
-(comment
-  ;;;
-  ;;; These show zero results with the pgbench example
-  ;;; show them once we can reproduce the required conditions
-  ;;;
-  ^:kindly/hide-code (meta-as-header #'pgex/partition-children)
-  (show (pgex/partition-children db))
+;; These show no results for the pgbench example
+;; but can be valuable in other scenarios.
 
-  ^:kindly/hide-code (meta-as-header #'pgex/partition-parents)
-  (show (pgex/partition-parents db))
+^:kindly/hide-code (meta-as-header #'pgex/partition-children)
+(show (pgex/partition-children db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/duplicate-indexes)
-  (show (pgex/duplicate-indexes db))
+^:kindly/hide-code (meta-as-header #'pgex/partition-parents)
+(show (pgex/partition-parents db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/locks)
-  (show (pgex/locks db))
+^:kindly/hide-code (meta-as-header #'pgex/duplicate-indexes)
+(show (pgex/duplicate-indexes db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/null-indexes)
-  (show (pgex/null-indexes db))
+^:kindly/hide-code (meta-as-header #'pgex/locks)
+(show (pgex/locks db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/all-locks)
-  (show (pgex/all-locks db))
+^:kindly/hide-code (meta-as-header #'pgex/null-indexes)
+(show (pgex/null-indexes db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/blocking)
-  (show (pgex/blocking db))
+^:kindly/hide-code (meta-as-header #'pgex/all-locks)
+(show (pgex/all-locks db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/unused-indexes)
-  (show (pgex/unused-indexes db {:min_scans 50}))
+^:kindly/hide-code (meta-as-header #'pgex/blocking)
+(show (pgex/blocking db))
 
-  ^:kindly/hide-code (meta-as-header #'pgex/long-running-queries)
-  (show (pgex/long-running-queries db)))
+^:kindly/hide-code (meta-as-header #'pgex/unused-indexes)
+(show (pgex/unused-indexes db {:min_scans 50}))
 
+^:kindly/hide-code (meta-as-header #'pgex/long-running-queries)
+(show (pgex/long-running-queries db))
 
 ;; ## The Kill Switch
 
@@ -319,9 +301,8 @@
 ;; vital if you need to take a heavily-used database
 ;; down for maintenance or emergency.
 ;;
-;;; ```clojure
-;;; (pgex/kill-all! db)  ;; use with caution
-;;; ```
+(comment
+  (pgex/kill-all! db))  ;; use with caution!
 
 ;; ## Diagnostics
 
